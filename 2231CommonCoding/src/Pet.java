a9541d4f4ad87303c16dc7d47d87c254fe1947dd
Kyle DeGuzman <48830018+KyleADeGuzman@users.noreply.github.com>
2023-05-05 23:04:21 -0400

    IT WORKS!
    final submission 1.1

--- 2231CommonCoding/src/Pet.java
+++ 2231CommonCoding/src/Pet.java
@@ -1,83 +1,106 @@
-//Stephen Colandro
 import java.time.LocalDate;
 
-public abstract class Pet {
-	    private String name;
-	    private LocalDate birthdate;
-	    private double price;
-	    private String speciesType;
-	    private String specialFeature;
-	    private Boolean specialFeatureBoolean;
-	    
-	    public Pet(String name, LocalDate birthdate, double price, String speciesType, String specialFeature) {
-	        this.name = name;
-	        this.birthdate = birthdate;
-	        this.price = price;
-	        this.speciesType = speciesType;
-	        this.specialFeature = specialFeature;
-	    }
-	    
-	    public Pet(String name, LocalDate birthdate, double price, String speciesType, boolean specialFeatureBoolean) {
-	    	this.name = name;
-	        this.birthdate = birthdate;
-	        this.price = price;
-	        this.speciesType = speciesType;
-	        this.specialFeatureBoolean = specialFeatureBoolean;
-	    }
-	    
-	    public String getName() {
-	        return name;
-	    }
-	    
-	    public void setName(String name) {
-	        this.name = name;
-	    }
-	    
-	    public LocalDate getBirthdate() {
-	        return birthdate;
-	    }
-	    
-	    public void setBirthdate(LocalDate birthdate) {
-	        this.birthdate = birthdate;
-	    }
-	    
-	    public double getPrice() {
-	        return price;
-	    }
-	    
-	    public void setPrice(double price) {
-	        this.price = price;
-	    }
-	    
-	    public String getSpeciesType() {
-	        return speciesType;
-	    }
-	    
-	    public void setSpeciesType(String speciesType) {
-	        this.speciesType = speciesType;
-	    }
-	    
-	    public String getSpecialFeature() {
-	        return specialFeature;
-	    }
-	    
-	    public void setSpecialFeature(String specialFeature) {
-	        this.specialFeature = specialFeature;
-	    }
-	    
-	    public boolean getSpecialFeatureBoolean() {
-	        return specialFeatureBoolean;
-	    }
-	    
-	    public void setSpecialFeatureBoolean(boolean specialFeatureBoolean) {
-	        this.specialFeatureBoolean = specialFeatureBoolean;
-	    }
-	    
-	    public String toString() {
-	        return name + " (" + speciesType + ")" + "\n" +
-	               "Birthdate: " + birthdate + "\n" +
-	               "Price: $" + price + "\n" +
-	               "Special Feature: " + specialFeature + "\n";
-	    }
-	 
+// Abstract Pet class
+abstract class Pet {
+    private String name;
+    private LocalDate birthdate;
+    private double price;
+    private String speciesType;
+
+    // Constructor
+    public Pet(String name, LocalDate birthdate, double price, String speciesType) {
+        this.name = name;
+        this.birthdate = birthdate;
+        this.price = price;
+        this.speciesType = speciesType;
+    }
+
+    // Accessor and mutator methods
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    public LocalDate getBirthdate() { return birthdate; }
+    public void setBirthdate(LocalDate birthdate) { this.birthdate = birthdate; }
+    public double getPrice() { return price; }
+    public void setPrice(double price) { this.price = price; }
+    public String getSpeciesType() { return speciesType; }
+    public void setSpeciesType(String speciesType) { this.speciesType = speciesType; }
+
+    // Abstract method to get special feature (to be implemented in subclasses)
+    public abstract String getSpecialFeature();
+
+    // toString method
+    public String toString() {
+        return "Name: " + name + ", Birthdate: " + birthdate + ", Price: " + price + ", Species Type: " + speciesType;
+    }
 }
+
+// Subclass for Dogs & Cats
+class DogCat extends Pet {
+    private String breed; // Special feature
+
+    public DogCat(String name, LocalDate birthdate, double price, String speciesType, String breed) {
+        super(name, birthdate, price, speciesType);
+        this.breed = breed;
+    }
+
+    public String getSpecialFeature() { return breed; }
+    public void setBreed(String breed) { this.breed = breed; }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", Breed: " + breed;
+    }
+}
+
+// Subclass for Reptiles
+class Reptile extends Pet {
+    private String bloodType; // Special feature
+
+    public Reptile(String name, LocalDate birthdate, double price, String speciesType, String bloodType) {
+        super(name, birthdate, price, speciesType);
+        this.bloodType = bloodType;
+    }
+
+    public String getSpecialFeature() { return bloodType; }
+    public void setBloodType(String bloodType) { this.bloodType = bloodType; }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", Blood Type: " + bloodType;
+    }
+}
+
+// Subclass for Birds
+class Bird extends Pet {
+    private String movementType; // Special feature
+
+    public Bird(String name, LocalDate birthdate, double price, String speciesType, String movementType) {
+        super(name, birthdate, price, speciesType);
+        this.movementType = movementType;
+    }
+
+    public String getSpecialFeature() { return movementType; }
+    public void setMovementType(String movementType) { this.movementType = movementType; }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", Movement Type: " + movementType;
+    }
+}
+
+// Subclass for Rabbits & Small Animals
+class RabbitSmallAnimal extends Pet {
+    private boolean itBites; // Special feature
+
+    public RabbitSmallAnimal(String name, LocalDate birthdate, double price, String speciesType,boolean itBites) {
+		super(name, birthdate, price, speciesType);
+		this.itBites = itBites;
+		}
+		public String getSpecialFeature() { return itBites ? "yes" : "no"; }
+		public void setItBites(boolean itBites) { this.itBites = itBites; }
+		
+		@Override
+		public String toString() {
+			return super.toString() + ", It Bites: " + getSpecialFeature();
+		}
+	}		
--- 2231CommonCoding/src/TestMyPetStore.java
+++ 2231CommonCoding/src/TestMyPetStore.java
@@ -1,118 +1,90 @@
-//Stephen Colandro
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.time.LocalDate;
-import java.time.format.DateTimeParseException;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Scanner;
+import java.util.Comparator;
+import java.util.List;
 
 public class TestMyPetStore {
-    
-    private static final int PETS_PER_PAGE = 40;
-    
-    public static void main(String[] args) {
-        // Read input file and create Pet objects
-        ArrayList<Pet> pets = readInputFile("input-2.txt");
-        
-        // Sort pets by price
-        Collections.sort(pets, new PetPriceComparator());
-        
-        // Print report
-        printReport(pets);
-    }
-    
-    private static boolean getSpecialFeatureBoolean(String specialFeature) {
-        return specialFeature.equalsIgnoreCase("Yes");
-    }
-    
-    private static ArrayList<Pet> readInputFile(String filename) {
-        ArrayList<Pet> pets = new ArrayList<>();
-        
-        try (Scanner scanner = new Scanner(new File(filename))) {
-            int numPets = scanner.nextInt();
-            scanner.nextLine(); 
-            
-            for (int i = 0; i < numPets; i++) {
-                String name = scanner.nextLine();
-                LocalDate birthdate = LocalDate.parse(scanner.nextLine());
-                double price = scanner.nextDouble();
-                scanner.nextLine(); 
-                String species = scanner.nextLine();
-                String specialFeature = scanner.nextLine();
-                
-                if (species.equals("Dog")) {
-                    pets.add(new Dogs(name, birthdate, price, species, specialFeature));
-                } 
-                
-                else if (species.equals("Cat")) {
-                    pets.add(new Cats(name, birthdate, price, species, specialFeature));
-                } 
-                
-                else if (species.equals("Reptile")) {
-                    pets.add(new Reptiles(name, birthdate, price, species, specialFeature));
-                } 
-                
-                else if (species.equals("Bird")) {
-                    pets.add(new Birds(name, birthdate, price, species, specialFeature));
-                } 
-                
-                else if (species.equals("Rabbit")) {
-                	boolean itBites = getSpecialFeatureBoolean(specialFeature);
-                    pets.add(new Rabbits(name, birthdate, price, species, itBites));
-                } 
-                
-                else if (species.equals("Small Animal")) {
-                	boolean itBites = getSpecialFeatureBoolean(specialFeature);;
-                    pets.add(new SmallAnimals(name, birthdate, price, species, itBites));
-                } 
-                
-                else {
-                    throw new IllegalArgumentException("Invalid species: " + species);
+    private static List<Pet> petList = new ArrayList<>(); 
+
+    // Method to read input file and store objects in the list of Pets
+    public static void readInputFile(String fileName) {
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            int numberOfPets = Integer.parseInt(br.readLine().trim());
+            String line;
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("M/d/yyyy");
+            while ((line = br.readLine()) != null) {
+                String[] data = line.split("\t");
+                String name = data[0];
+                LocalDate birthdate = LocalDate.parse(data[1], formatter);
+                double price = Double.parseDouble(data[2]);
+                String speciesType = data[3];
+                String specialFeature = data[4];
+                Pet pet = null;
+                switch (speciesType) {
+                    case "Dog":
+                    case "Cat":
+                        pet = new DogCat(name, birthdate, price, speciesType, specialFeature);
+                        break;
+                    case "Reptile":
+                        pet = new Reptile(name, birthdate, price, speciesType, specialFeature);
+                        break;
+                    case "Bird":
+                        pet = new Bird(name, birthdate, price, speciesType, specialFeature);
+                        break;
+                    case "Rabbit":
+                    case "Small Animal":
+                        boolean itBites = specialFeature.equalsIgnoreCase("yes");
+                        pet = new RabbitSmallAnimal(name, birthdate, price, speciesType, itBites);
+                        break;
+                }
+                if (pet != null) {
+                petList.add(pet); 
                 }
             }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        
-        //Catches
-        catch (FileNotFoundException e) {
-            System.err.println("Error: input file not found");
-        } 
-        
-        catch (DateTimeParseException | NumberFormatException e) {
-            System.err.println("Error: invalid input values in input file");
-        }
+    }
 
-        //Check if specialFeature is empty and set to "N/A" if it is
-        for (Pet pet : pets) {
-            if (pet.getSpecialFeature().isEmpty()) {
-                pet.setSpecialFeature("N/A");
-            }
-        }
-        
-        return pets;
+    // Method to sort the list of Pets by price in ascending order
+    public static void sortPetsByPrice() {
+        petList.sort(Comparator.comparingDouble(Pet::getPrice));
     }
-    
-    private static void printReport(ArrayList<Pet> pets) {
-        int numPages = (int) Math.ceil((double) pets.size() / PETS_PER_PAGE);
-        
-        for (int pageNum = 1; pageNum <= numPages; pageNum++) {
-            //Print report header
-            System.out.println("MyPetStore Report - Page " + pageNum);
-            System.out.printf("%-20s%-15s%-10s%-20s%s%n",
-                    "Name", "Birthdate", "Price", "Type", "Special Feature");
-            System.out.println("-------------------------------------------------------------");
-            
-            //Print pets for this page
-            int startIndex = (pageNum - 1) * PETS_PER_PAGE;
-            int endIndex = Math.min(startIndex + PETS_PER_PAGE, pets.size());
-            for (int i = startIndex; i < endIndex; i++) {
-                System.out.println(pets.get(i));
+
+    // Method to format and print the report
+    public static void printReport() {
+        int petsPerPage = 40;
+        int pageNumber = 1;
+        int petCounter = 0;
+        for (Pet pet : petList) { 
+            if (petCounter % petsPerPage == 0) {
+                if (petCounter > 0) {
+                    System.out.println("End of Page " + pageNumber++);
+                }
+                System.out.println("Page " + pageNumber);
+                System.out.println("-------------------------------------------------");
+                System.out.printf("%-20s %-15s %-10s %-15s %-15s%n", "Name", "Birthdate", "Price", "Species", "Feature");
+                System.out.println("-------------------------------------------------");
             }
-            
-            //Print page footer
-            System.out.println("-------------------------------------------------------------");
-            System.out.printf("Page %d of %d%n%n", pageNum, numPages);
+            System.out.printf("%-20s %-15s %-10.2f %-15s %-15s%n", pet.getName(), pet.getBirthdate(), pet.getPrice(), pet.getSpeciesType(), pet.getSpecialFeature());
+            petCounter++;
+        }
+        if (petCounter > 0) {
+            System.out.println("End of Page " + pageNumber);
         }
     }
-    
+
+    // Main method to execute the program
+    public static void main(String[] args) {
+        // Read input file and store objects in the list
+        readInputFile("input-2.txt"); // Provide the correct path to the
+
+    sortPetsByPrice();
+
+    printReport();
 }
+}            
